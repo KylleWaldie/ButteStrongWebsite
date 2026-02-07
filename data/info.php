5e362f3e4daf83c82b6d8e72c3c1416637c537c1
KylleWaldie <wldkylle@gmail.com>
2026-02-07 11:20:27 -0700

    Where data is stored. Contanct info, important links ect.

--- data/info.php
+++ data/info.php
@@ -1,5 +1,6 @@
 <?php
 return [
     'home_message' => 'Welcome to our website!',
-    'immigration_link' => 'First link go here.'
+    'immigration_link' => 'First link go here.',
+    'link_2' => 'This is a second link spot'
 ];
