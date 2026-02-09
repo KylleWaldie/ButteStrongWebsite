205b891e28e802b43c454ef1d6faa13ea524288e
Kylle Waldie <wldkylle@gmail.com>
2026-02-08 21:38:53 -0700

    Added links for social media if they have any

--- data/info.php
+++ data/info.php
@@ -2,5 +2,8 @@
 return [
     'home_message' => 'Welcome to our website!',
     'immigration_link' => 'First link go here.',
-    'link_2' => 'This is a second link spot'
+    'link_2' => 'This is a second link spot',
+    'facebook_link' => 'https://facebook.com',
+    'x_link' => 'https://x.com',
+    'instagram_link' => 'https://instagram.com',
 ];
