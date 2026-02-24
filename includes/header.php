b976b095dd6b495db3da7a0f92bfd06dc140b16f
PWeber-tech <pweber@mtech.edu>
2026-02-24 15:24:16 -0700

    halfway finished with mobile implementation.

--- includes/header.php
+++ includes/header.php
@@ -1,14 +1,18 @@
 <header class="site-header">
-    <h1>Butte Tough</h1>
-    <nav class="sidebar">
-        <ul>
-            <li><a href="/../public/index.php">Home</a></li>
-            <li><a href="/../public/importantLinks.php">Important Links</a></li>
-            <li><a href="/../public/contact_page.php">Contact</a></li>
-        </ul>
-    </nav>
+    <div class="navbar">
+        <button class="nav_burger">menu</button>
+        <nav class="sidebar">
+            <ul data-visible="false">
+                <li><a href="/../public/index.php"><div class="navlink"><h1>Butte Tough</h1></div></a></li>
+                <li><a href="/../public/index.php"><div class="navlink">Resource 1</div></a></li>
+                <li><a href="/../public/importantLinks.php"><div class="navlink">Resource 2</div></a></li>
+                <li><a href="/../public/contact_page.php"><div class="navlink">Resource 3</div></a></li>
+            </ul>
+        </nav>
+    </div>
 
     <!-- Cookie Banner -->
+     <!--
     <div id="cookie-banner">
         This site uses cookies to track visitors and improve user experience.
         <button onclick="document.getElementById('cookie-banner').style.display='none'" 
@@ -16,7 +20,7 @@
             OK
         </button>
     </div>
-
+-->
     <!-- Load cookie JS -->
     <script src="/../public/js/cookies.js"></script>
 </header>
--- public/index.php
+++ public/index.php
@@ -9,9 +9,12 @@ $info = include __DIR__ . '/../data/info.php';
     <head>
         <title>Home Page</title>
         <link rel="stylesheet" href="styles.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
 
+
     <body>
+        <div class=encompass>
         <?php include __DIR__ . '/../includes/header.php'; ?>
         <main class="content">
             <div class="home_image">
@@ -23,9 +26,14 @@ $info = include __DIR__ . '/../data/info.php';
             <div class="home-content">
                 <p>If you are danger call emergency services (911) <br>
                 To report something to Butte Tough text or call this number (000) 000-0000</p>
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                <p>hello</p>
             </div>
         </main>
+        </div>
+        <!--
         <?php include __DIR__ . '/../includes/footer.php'; ?>
+-->
     </body>
 
 </html>
