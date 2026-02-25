428fd2a41660152a6dfc3e7a340ebc2b4793fd17
Kylle Waldie <wldkylle@gmail.com>
2026-02-25 00:36:04 -0700

    Fixed linking issues. Have to change later

--- includes/header.php
+++ includes/header.php
@@ -1,29 +1,15 @@
 <header class="site-header">
     <div class="navbar">
         <div class="topper">
-        <a href="/../public/index.php"><div class="logolink"><img src="logo.png" class="logo" alt="Description of the image"></div></a>
+        <a href="/ButteToughWebsite/public/index.php"><div class="logolink"><img src="/ButteToughWebsite/public/images/logo-removebg-preview.png" class="logo" alt="Butte Strong Logo"></div></a>
         <button class="nav_burger">☰</button>
         </div>
         <nav class="sidebar">
             <ul data-visible="false">
-                <li><a href="/../public/index.php"><div class="navlink">Resource 1</div></a></li>
-                <li><a href="/../public/importantLinks.php"><div class="navlink">Resource 2</div></a></li>
-                <li><a href="/../public/contact_page.php"><div class="navlink">Resource 3</div></a></li>
+                <li><a href="/ButteToughWebsite/public/index.php"><div class="navlink">Resource 1</div></a></li>
+                <li><a href="/ButteToughWebsite/public/importantLinks.php"><div class="navlink">Resource 2</div></a></li>
+                <li><a href="/ButteToughWebsite/public/contact_page.php"><div class="navlink">Resource 3</div></a></li>
             </ul>
         </nav>
     </div>
-
-    <script src="events.js"></script>
-    <!-- Cookie Banner -->
-     <!--
-    <div id="cookie-banner">
-        This site uses cookies to track visitors and improve user experience.
-        <button onclick="document.getElementById('cookie-banner').style.display='none'" 
-                style="margin-left:10px; padding:5px 10px;">
-            OK
-        </button>
-    </div>
--->
-    <!-- Load cookie JS -->
-    <script src="/../public/js/cookies.js"></script>
 </header>
