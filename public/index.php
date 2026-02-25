1a4b1e1bfa98d73335fa17bb386a80040c2b9e09
Kylle Waldie <wldkylle@gmail.com>
2026-02-25 01:50:01 -0700

    Moved footer into nav bar for now for testing now.

--- public/index.php
+++ public/index.php
@@ -14,22 +14,22 @@ $info = include __DIR__ . '/../data/info.php';
 
     <body>
         <div class=encompass>
-        <?php include __DIR__ . '/../includes/header.php'; ?>
-        <main class="content">
-            <div class="home_image">
-                <img src="/ButteToughWebsite/public/images/ButtePhoto.png" alt="Home Image">
-            </div>
-            <div class="welcome-message">
-               <h1><?= htmlspecialchars($info['home_message']) ?></h1> 
-            </div>
-            <div class="home-content">
-                <p>If you are danger call emergency services (911) <br>
-                To report something to Butte Tough text or call this number (000) 000-0000</p>
-            </div>
-            <p class="home-content">This website is for informational purposes only and is not a substitute for professional advice or emergency services.</p>
-        </main>
+            <?php include __DIR__ . '/../includes/header.php'; ?>
+            <main class="content">
+                <div class="home_image">
+                    <img src="/ButteToughWebsite/public/images/ButtePhoto.png" alt="Home Image">
+                </div>
+                <div class="welcome-message">
+                <h1><?= htmlspecialchars($info['home_message']) ?></h1> 
+                </div>
+                <div class="home-content">
+                    <p>If you are danger call emergency services (911) <br>
+                    To report something to Butte Tough text or call this number (000) 000-0000</p>
+                </div>
+                <p class="home-content">This website is for informational purposes only and is not a substitute for professional advice or emergency services.</p>
+            </main>
+            <?php include __DIR__ . '/../includes/footer.php'; ?>
         </div>
-        <?php include __DIR__ . '/../includes/footer.php'; ?>
     </body>
 
 </html>
